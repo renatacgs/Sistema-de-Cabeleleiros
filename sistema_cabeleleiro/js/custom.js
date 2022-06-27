36d46d4514a15da688e87322eaa4642baa2a5c92
Renata Cristina <renatinha.8@hotmail.com>
2022-06-26 21:51:04 -0300

    Add files via upload

---
+++ sistema_cabeleleiro/js/custom.js
@@ -0,0 +1,167 @@
+(function($) {
+    "use strict";
+
+	$(window).on('load', function() {
+		$('.preloader').fadeOut();
+		$('#preloader').delay(550).fadeOut('slow');
+		$('body').delay(450).css({
+			'overflow': 'visible'
+		});
+	});
+
+
+
+	$(window).on('scroll', function() {
+		if ($(window).scrollTop() > 50) {
+			$('.main-header').addClass('fixed-menu');
+		} else {
+			$('.main-header').removeClass('fixed-menu');
+		}
+	});
+
+
+
+	$('#slides-Mais informações').superslides({
+		inherit_width_from: '.cover-slides',
+		inherit_height_from: '.cover-slides',
+		play: 5000,
+		animation: 'fade',
+	});
+
+	$(".cover-slides ul li").append("<div class='overlay-background'></div>");
+
+
+
+	$(document).ready(function() {
+		$(window).on('scroll', function() {
+			if ($(this).scrollTop() > 100) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		$('#back-to-top').click(function() {
+			$("html, body").animate({
+				scrollTop: 0
+			}, 600);
+			return false;
+		});
+	});
+
+
+
+	var Container = $('.container');
+	Container.imagesLoaded(function() {
+		var portfolio = $('.special-menu');
+		portfolio.on('click', 'button', function() {
+			$(this).addClass('active').siblings().removeClass('active');
+			var filterValue = $(this).attr('data-filter');
+			$grid.isotope({
+				filter: filterValue
+			});
+		});
+		var $grid = $('.special-list').isotope({
+			itemSelector: '.special-grid'
+		});
+	});
+
+
+
+	$(document).ready(function() {
+		$('[data-toggle="tooltip"]').tooltip();
+	});
+
+
+
+	$('.main-instagram').owlCarousel({
+		loop: true,
+		margin: 0,
+		dots: false,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true,
+		navText: ["<i class='fas fa-arrow-left'></i>", "<i class='fas fa-arrow-right'></i>"],
+		responsive: {
+			0: {
+				items: 2,
+				nav: true
+			},
+			600: {
+				items: 3,
+				nav: true
+			},
+			1000: {
+				items: 5,
+				nav: true,
+				loop: true
+			}
+		}
+	});
+
+
+
+	$('.featured-products-box').owlCarousel({
+		loop: true,
+		margin: 15,
+		dots: false,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true,
+		navText: ["<i class='fas fa-arrow-left'></i>", "<i class='fas fa-arrow-right'></i>"],
+		responsive: {
+			0: {
+				items: 1,
+				nav: true
+			},
+			600: {
+				items: 3,
+				nav: true
+			},
+			1000: {
+				items: 4,
+				nav: true,
+				loop: true
+			}
+		}
+	});
+
+
+	$(document).ready(function() {
+		$(window).on('scroll', function() {
+			if ($(this).scrollTop() > 100) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		$('#back-to-top').click(function() {
+			$("html, body").animate({
+				scrollTop: 0
+			}, 600);
+			return false;
+		});
+	});
+
+
+	$(function() {
+		$("#slider-range").slider({
+			range: true,
+			min: 0,
+			max: 4000,
+			values: [1000, 3000],
+			slide: function(event, ui) {
+				$("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+			}
+		});
+		$("#amount").val("$" + $("#slider-range").slider("values", 0) +
+			" - $" + $("#slider-range").slider("values", 1));
+	});
+
+
+
+	$(".brand-box").niceScroll({
+		cursorcolor: "#9b9b9c",
+	});
+	
+	
+}(jQuery));
